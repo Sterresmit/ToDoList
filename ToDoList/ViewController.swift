e3cfc891fdef06b05086edae66534d22e07d5039
Sterre <sterresmit98@gmail.com>
2018-11-28 00:02:04 +0100

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Sterre Smit on 28/11/2018.
+//  Copyright © 2018 Sterre Smit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
